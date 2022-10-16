f64418359bf66a93f3e69efba5e7a1b9e1871078
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-10-16 13:31:01 +0100

    useEffect()

--- components/ManualHeader.js
+++ components/ManualHeader.js
@@ -1,6 +1,14 @@
+import { useEffect } from 'react'
 import { useMoralis } from 'react-moralis'
 export default function ManualHeader() {
-  const { enableWeb3, account } = useMoralis()
+  const { enableWeb3, account, isWeb3Enabled } = useMoralis()
+  useEffect(() => {
+    console.log('useEffect.....')
+    console.log(isWeb3Enabled)
+  }, [])
+  // no dependency array : run anytime something re-renders
+  // CAREFUL with this!! because you can get circular render
+  // blnnk dependency array, only run once.
 
   return (
     <div>
