f4868ea290aa2fbfc7ec9c5159810c2815416101
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-10-20 14:17:47 +0100

    Exporting constants

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -1,5 +1,7 @@
 // Creating a function to enter the lottery
 import { useWeb3Contract } from 'react-moralis'
+import { abi, contractAddressess } from '../constants'
+
 export default function LotteryEntrance() {
   // const {runContractFunction: raffle} = useWeb3Contract({
   // 	abi: //,
