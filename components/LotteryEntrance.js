81881bae947d65b54fafddd8b0e993275d3bc202
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-10-21 15:36:26 +0100

    Use State

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -1,14 +1,17 @@
 // Creating a function to enter the lottery
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useWeb3Contract } from 'react-moralis'
 import { abi, contractAddresses } from '../constants'
 import { useMoralis } from 'react-moralis'
+import { ethers } from 'ethers'
 
 export default function LotteryEntrance() {
   const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
   const chainId = parseInt(chainIdHex)
   const raffleAddress =
     chainId in contractAddresses ? contractAddresses[chainId][0] : null
+
+  const { entranceFee, setEntranceFee } = useState('0')
 
   // const { runContractFunction: raffle } = useWeb3Contract({
   //   abi: abi,
@@ -29,12 +32,13 @@ export default function LotteryEntrance() {
     if (isWeb3Enabled) {
       // try to read the raffle entrance fee
       async function updatedUI() {
-        const entranceFeeFromContract = await getEntranceFee()
-        console.log(entranceFeeFromContract)
+        const entranceFeeFromCall = (await getEntranceFee()).toString()
+        setEntranceFee(ethers.utils.formatUnits(entranceFeeFromCall, 'ethers'))
+        console.log(entranceFee)
       }
       updatedUI()
     }
   }, [getEntranceFee, isWeb3Enabled])
 
-  return <div>Hello from Lottery Entrance</div>
+  return <div>Hello from Lottery Entrance, Entrance Fee:{entranceFee} </div>
 }
